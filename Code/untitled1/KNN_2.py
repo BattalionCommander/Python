22e3131820e7b8121d78f37e760c16316dea4594
BattalionCommander <835809508@qq.com>
2018-06-13 11:25:09 +0800

    1

--- Code/untitled1/KNN_2.py
+++ Code/untitled1/KNN_2.py
@@ -8,9 +8,17 @@ from sklearn import neighbors, datasets
 #分类距离
 n_neighbors = 15
 
-# import some data to play with
+
 # 导入一些数据
 iris = datasets.load_iris()
+print(iris.data.shape())
+print(iris.target.shape())
+
+# iris中文指鸢尾植物，这里存储了其萼片和花瓣的长和宽，一共4个属性。鸢尾植物又分三类。
+# 与之相对，iris里有两个属性iris.data，iris.target
+# data里是一个矩阵，每一列代表了萼片或花瓣的长宽，一共4列，每一列代表某个被测量的鸢尾植物，一共采样了150条记录，所以查看这个矩阵的形状iris.data.shape，
+#target是一个数组，存储了data中每条记录属于哪一类鸢尾植物，所以数组的长度是150，数组元素的值因为共有3类鸢尾植物，所以不同值只有3个。
+
 
 
 # 我们只取前两个特征。
